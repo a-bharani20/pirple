483f05c58bcb67997128b0da9f38f180e1a5a0ae
a-bharani20 <bharanidharan.anantharaman@gmail.com>
2018-12-15 17:51:08 +0530

    Homework# #1

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Homework Assignment#1
+This main.py homework prints the attributes of a song
+Created by: Bharanidharan A
+"""
+
+#Title of the song
+title = "Endlessly"
+print(title)
+
+#Album
+album = "Absolution"
+print(album)
+
+#Artist
+artist = "muse"
+print(artist
+)
+#Year
+year = 2003
+print(year)
+
+#Genre
+genre = "Rock"
+print(genre)
+
+#Track
+track = 12
+print(track)
+
+
